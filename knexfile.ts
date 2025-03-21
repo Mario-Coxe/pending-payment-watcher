065cbcb5783e159204a80c2479828b0d276c4258
mario.coxe <mario.coxe@ideiasdinamicas.com>
2025-03-21 23:13:12 +0100

    feat: add another mysql client for connection

--- knexfile.ts
+++ knexfile.ts
@@ -1,13 +1,17 @@
-import knex from 'knex';
+import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = knex({
-  client: 'mysql2', 
+  client: process.env.CLIENT_MYSQL,
   connection: {
-    host: '',
-    user: '',
-    password: '',
-    database: '',
-  },
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT)
+  }
 });
 
 export default db;
